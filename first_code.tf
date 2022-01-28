93ec2075a14afc00b36a796ffeac63a42636dcbe
Malcolm Safo <malcolmsafo@gmail.com>
2022-01-28 22:35:42 +0000

    edit region

--- first_code.tf
+++ first_code.tf
@@ -1,11 +1,9 @@
 provider "aws" {
   profile = "default"
-  region  = "us-west-2"
+  region  = "us-east-1"
 }
 
 resource "aws_s3_bucket" "tf_bonsai" {
   bucket = "tf-bonsai-20220128"
   acl    = "private"
 }
-
-
