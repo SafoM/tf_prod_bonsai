c89d35c89476508416fb243a4ec99c99a7be1147
Malcolm Safo <malcolmsafo@gmail.com>
2022-01-29 03:00:31 +0000

    Use eip association

--- prod.tf
+++ prod.tf
@@ -50,9 +50,14 @@ resource "aws_instance" "prod_web" {
     "Terraform" : "true"
   }
 }
-resource "aws_eip" "prod_web" {
-  instance = aws_instance.prod_web.id
 
+resource "aws_eip_association" "prod_web" {
+  instance_id   = aws_instance.prod_web.id
+  allocation_id = aws_eip.prod_web.id
+
+}
+
+resource "aws_eip" "prod_web" {
   tags = {
     "Terraform" : "true"
   }
