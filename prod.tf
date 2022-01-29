f6d9023e3a478ff61222df80e5795266a6adebfa
Malcolm Safo <malcolmsafo@gmail.com>
2022-01-29 02:51:30 +0000

    web instances and elastic ip

--- prod.tf
+++ prod.tf
@@ -9,3 +9,51 @@ resource "aws_s3_bucket" "prod_tf_bonsai" {
 }
 
 resource "aws_default_vpc" "default" {}
+
+resource "aws_security_group" "prod_web" {
+  name        = "prod_web"
+  description = ""
+
+  ingress {
+   from_port   = 80
+   to_port     = 80
+   protocol    = "tcp"
+   cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+   from_port   = 443
+   to_port     = 443
+   protocol    = "tcp"
+   cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    "Terraform" : "true"
+  }
+}
+
+resource "aws_instance" "prod_web" {
+  ami           = "ami-05343502b4149e010"
+  instance_type = "t3a.small"
+
+  vpc_security_group_ids = [
+    aws_security_group.prod_web.id
+  ]
+
+  tags = {
+    "Terraform" : "true"
+  }
+}
+resource "aws_eip" "prod_web" {
+  instance = aws_instance.prod_web.id
+
+  tags = {
+    "Terraform" : "true"
+  }
+}
